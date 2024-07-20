2bb88455142febfdfb92cf3d878a36c69ad7f7d2
VeerioSDSC <veerchopraalt@gmail.com>
2024-07-19 22:33:27 -0700

    Update install.py

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
 import micropip
 whl_url = 'https://g-062a3c.0ed28.75bc.data.globus.org/public/healpy-0.1-py3-none-any.whl'
 micropip.install(whl_url)
-await micropip.install("matplotlib")
+micropip.install("matplotlib")
 import healpy
 import numpy
